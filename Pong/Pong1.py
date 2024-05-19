d1dea921be7ff9c7793a77151fd7b195c735a8b7
AzorAhoy <automotba@gmail.com>
2024-05-19 18:13:08 +0700

    pong

--- Pong/Pong1.py
+++ Pong/Pong1.py
@@ -1,9 +1,15 @@
 import turtle
+import winsound
+import os 
 wn = turtle.Screen()
 wn.title("PONG")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+
+# score
+score_a = 0
+score_b = 0
 
 # Paddle A
 paddle_a = turtle.Turtle()
@@ -73,6 +79,7 @@ wn.onkeypress(paddle_b_down,"Down")
 # Main loop
 while True:
     wn.update()
+    # winsound.PlaySound("D:/Python Games/Pong/bounce.wav", winsound.SND_ASYNC)
 
     # move ball
     ball.setx(ball.xcor() + ball.dx)
@@ -82,25 +89,34 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        winsound.PlaySound('bounce.wav', winsound.SND_ALIAS)
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        winsound.PlaySound('bounce.wav', winsound.SND_ALIAS)
 
     if ball.xcor() > 390:
         ball.goto(0,0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}, Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0,0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}, Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     # ball & paddle collisions
     if ball.xcor() > 340 and ball.xcor() < 350 and ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40:
         ball.setx(340)
         ball.dx *= -1
+        winsound.PlaySound('bounce.wav', winsound.SND_ALIAS)
 
     if ball.xcor() < -340 and ball.xcor() > -350 and ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40:
         ball.setx(-340)
         ball.dx *= -1
-
+        winsound.PlaySound('bounce.wav', winsound.SND_ALIAS)
---
+++ Pong/testSound.py
@@ -0,0 +1,8 @@
+import winsound
+import time
+while True:
+    # winsound.PlaySound("D:/Python Games/bounce.wav", winsound.SND_ASYNC)
+    # winsound.Beep(440, 1000)
+    winsound.PlaySound("bounce.wav", winsound.SND_ALIAS)
+    # time.sleep(1)    
+    # winsound.Beep(440, 1000)
